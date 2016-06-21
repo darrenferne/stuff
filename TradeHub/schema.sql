3697814d8f2f3656c025f5e4c4907661089ba34d
darrenferne <darren.ferne@bradyplc.com>
2016-06-21 16:41:14 +0100

    SQL Server DB schema

--- TradeHub/schema.sql
+++ TradeHub/schema.sql
@@ -1,12 +1,60 @@
 USE [trading]
 GO
 
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[brady_trading].[commoditycarry]') AND type in (N'U'))
+   DROP TABLE [brady_trading].[commoditycarry]
+GO
+
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[brady_trading].[commoditytapo]') AND type in (N'U'))
+   DROP TABLE [brady_trading].[commoditytapo]
+GO
+
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[brady_trading].[commodityoption]') AND type in (N'U'))
+   DROP TABLE [brady_trading].[commodityoption]
+GO
+
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[brady_trading].[commodityaverageswap]') AND type in (N'U'))
+   DROP TABLE [brady_trading].[commodityaverageswap]
+GO
+
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[brady_trading].[commodityaverage]') AND type in (N'U'))
+   DROP TABLE [brady_trading].[commodityaverage]
+GO
+
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[brady_trading].[commodityforward]') AND type in (N'U'))
+   DROP TABLE [brady_trading].[commodityforward]
+GO
+
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[brady_trading].[commodityfuture]') AND type in (N'U'))
+   DROP TABLE [brady_trading].[commodityfuture]
+GO
+
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[brady_trading].[commoditytrade]') AND type in (N'U'))
+   DROP TABLE [brady_trading].[commoditytrade]
+GO
+
 IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[brady_trading].[trade]') AND type in (N'U'))
    DROP TABLE [brady_trading].[trade]
 GO
 
 IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[brady_trading].[nexthigh]') AND type in (N'U'))
    DROP TABLE [brady_trading].[nexthigh]
+GO
+
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[brady_trading].[vanillaaverage]') AND type in (N'U'))
+   DROP TABLE [brady_trading].[vanillaaverage]
+GO
+
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[brady_trading].[averagedetails]') AND type in (N'U'))
+   DROP TABLE [brady_trading].[averagedetails]
+GO
+
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[brady_trading].[vanillaoption]') AND type in (N'U'))
+   DROP TABLE [brady_trading].[vanillaoption]
+GO
+
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[brady_trading].[optiondetails]') AND type in (N'U'))
+   DROP TABLE [brady_trading].[optiondetails]
 GO
 
 IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[brady_trading].[schemaversion]') AND type in (N'U'))
@@ -28,6 +76,273 @@ GO
 CREATE SCHEMA [brady_trading] AUTHORIZATION [dbo]
 GO
 
+DECLARE @RoleName sysname
+set @RoleName = N'brady_trading_insert'
+IF  EXISTS (SELECT * FROM sys.database_principals WHERE name = @RoleName AND type = 'R')
+Begin
+	DECLARE @RoleMemberName sysname
+	DECLARE Member_Cursor CURSOR FOR
+	select [name]
+	from sys.database_principals 
+	where principal_id in ( 
+		select member_principal_id 
+		from sys.database_role_members 
+		where role_principal_id in (
+			select principal_id
+			FROM sys.database_principals where [name] = @RoleName  AND type = 'R' ))
+
+	OPEN Member_Cursor;
+
+	FETCH NEXT FROM Member_Cursor
+	into @RoleMemberName
+
+	WHILE @@FETCH_STATUS = 0
+	BEGIN
+
+		exec sp_droprolemember @rolename=@RoleName, @membername= @RoleMemberName
+
+		FETCH NEXT FROM Member_Cursor
+		into @RoleMemberName
+	END;
+
+	CLOSE Member_Cursor;
+	DEALLOCATE Member_Cursor;
+End
+
+GO
+
+IF  EXISTS (SELECT * FROM sys.database_principals WHERE name = N'brady_trading_insert' AND type = 'R')
+	DROP ROLE [brady_trading_insert]
+GO
+
+CREATE ROLE [brady_trading_insert] AUTHORIZATION [dbo]
+GO
+
+DECLARE @RoleName sysname
+set @RoleName = N'brady_trading_update'
+IF  EXISTS (SELECT * FROM sys.database_principals WHERE name = @RoleName AND type = 'R')
+Begin
+	DECLARE @RoleMemberName sysname
+	DECLARE Member_Cursor CURSOR FOR
+	select [name]
+	from sys.database_principals 
+	where principal_id in ( 
+		select member_principal_id 
+		from sys.database_role_members 
+		where role_principal_id in (
+			select principal_id
+			FROM sys.database_principals where [name] = @RoleName  AND type = 'R' ))
+
+	OPEN Member_Cursor;
+
+	FETCH NEXT FROM Member_Cursor
+	into @RoleMemberName
+
+	WHILE @@FETCH_STATUS = 0
+	BEGIN
+
+		exec sp_droprolemember @rolename=@RoleName, @membername= @RoleMemberName
+
+		FETCH NEXT FROM Member_Cursor
+		into @RoleMemberName
+	END;
+
+	CLOSE Member_Cursor;
+	DEALLOCATE Member_Cursor;
+End
+
+GO
+
+IF  EXISTS (SELECT * FROM sys.database_principals WHERE name = N'brady_trading_update' AND type = 'R')
+	DROP ROLE [brady_trading_update]
+GO
+
+CREATE ROLE [brady_trading_update] AUTHORIZATION [dbo]
+GO
+
+DECLARE @RoleName sysname
+set @RoleName = N'brady_trading_delete'
+IF  EXISTS (SELECT * FROM sys.database_principals WHERE name = @RoleName AND type = 'R')
+Begin
+	DECLARE @RoleMemberName sysname
+	DECLARE Member_Cursor CURSOR FOR
+	select [name]
+	from sys.database_principals 
+	where principal_id in ( 
+		select member_principal_id 
+		from sys.database_role_members 
+		where role_principal_id in (
+			select principal_id
+			FROM sys.database_principals where [name] = @RoleName  AND type = 'R' ))
+
+	OPEN Member_Cursor;
+
+	FETCH NEXT FROM Member_Cursor
+	into @RoleMemberName
+
+	WHILE @@FETCH_STATUS = 0
+	BEGIN
+
+		exec sp_droprolemember @rolename=@RoleName, @membername= @RoleMemberName
+
+		FETCH NEXT FROM Member_Cursor
+		into @RoleMemberName
+	END;
+
+	CLOSE Member_Cursor;
+	DEALLOCATE Member_Cursor;
+End
+
+GO
+
+IF  EXISTS (SELECT * FROM sys.database_principals WHERE name = N'brady_trading_delete' AND type = 'R')
+	DROP ROLE [brady_trading_delete]
+GO
+
+CREATE ROLE [brady_trading_delete] AUTHORIZATION [dbo]
+GO
+
+DECLARE @RoleName sysname
+set @RoleName = N'brady_trading_select'
+IF  EXISTS (SELECT * FROM sys.database_principals WHERE name = @RoleName AND type = 'R')
+Begin
+	DECLARE @RoleMemberName sysname
+	DECLARE Member_Cursor CURSOR FOR
+	select [name]
+	from sys.database_principals 
+	where principal_id in ( 
+		select member_principal_id 
+		from sys.database_role_members 
+		where role_principal_id in (
+			select principal_id
+			FROM sys.database_principals where [name] = @RoleName  AND type = 'R' ))
+
+	OPEN Member_Cursor;
+
+	FETCH NEXT FROM Member_Cursor
+	into @RoleMemberName
+
+	WHILE @@FETCH_STATUS = 0
+	BEGIN
+
+		exec sp_droprolemember @rolename=@RoleName, @membername= @RoleMemberName
+
+		FETCH NEXT FROM Member_Cursor
+		into @RoleMemberName
+	END;
+
+	CLOSE Member_Cursor;
+	DEALLOCATE Member_Cursor;
+End
+
+GO
+
+IF  EXISTS (SELECT * FROM sys.database_principals WHERE name = N'brady_trading_select' AND type = 'R')
+	DROP ROLE [brady_trading_select]
+GO
+
+CREATE ROLE [brady_trading_select] AUTHORIZATION [dbo]
+GO
+
+DECLARE @RoleName sysname
+set @RoleName = N'brady_trading_execute'
+IF  EXISTS (SELECT * FROM sys.database_principals WHERE name = @RoleName AND type = 'R')
+Begin
+	DECLARE @RoleMemberName sysname
+	DECLARE Member_Cursor CURSOR FOR
+	select [name]
+	from sys.database_principals 
+	where principal_id in ( 
+		select member_principal_id 
+		from sys.database_role_members 
+		where role_principal_id in (
+			select principal_id
+			FROM sys.database_principals where [name] = @RoleName  AND type = 'R' ))
+
+	OPEN Member_Cursor;
+
+	FETCH NEXT FROM Member_Cursor
+	into @RoleMemberName
+
+	WHILE @@FETCH_STATUS = 0
+	BEGIN
+
+		exec sp_droprolemember @rolename=@RoleName, @membername= @RoleMemberName
+
+		FETCH NEXT FROM Member_Cursor
+		into @RoleMemberName
+	END;
+
+	CLOSE Member_Cursor;
+	DEALLOCATE Member_Cursor;
+End
+
+GO
+
+IF  EXISTS (SELECT * FROM sys.database_principals WHERE name = N'brady_trading_execute' AND type = 'R')
+	DROP ROLE [brady_trading_execute]
+GO
+
+CREATE ROLE [brady_trading_execute] AUTHORIZATION [dbo]
+GO
+
+DECLARE @RoleName sysname
+set @RoleName = N'brady_trading_user'
+IF  EXISTS (SELECT * FROM sys.database_principals WHERE name = @RoleName AND type = 'R')
+Begin
+	DECLARE @RoleMemberName sysname
+	DECLARE Member_Cursor CURSOR FOR
+	select [name]
+	from sys.database_principals 
+	where principal_id in ( 
+		select member_principal_id 
+		from sys.database_role_members 
+		where role_principal_id in (
+			select principal_id
+			FROM sys.database_principals where [name] = @RoleName  AND type = 'R' ))
+
+	OPEN Member_Cursor;
+
+	FETCH NEXT FROM Member_Cursor
+	into @RoleMemberName
+
+	WHILE @@FETCH_STATUS = 0
+	BEGIN
+
+		exec sp_droprolemember @rolename=@RoleName, @membername= @RoleMemberName
+
+		FETCH NEXT FROM Member_Cursor
+		into @RoleMemberName
+	END;
+
+	CLOSE Member_Cursor;
+	DEALLOCATE Member_Cursor;
+End
+
+GO
+
+IF  EXISTS (SELECT * FROM sys.database_principals WHERE name = N'brady_trading_user' AND type = 'R')
+	DROP ROLE [brady_trading_user]
+GO
+
+CREATE ROLE [brady_trading_user] AUTHORIZATION [dbo]
+GO
+
+EXEC sp_addrolemember brady_trading_insert, brady_trading_user
+GO
+
+EXEC sp_addrolemember brady_trading_insert, brady_trading_user
+GO
+
+EXEC sp_addrolemember brady_trading_insert, brady_trading_user
+GO
+
+EXEC sp_addrolemember brady_trading_insert, brady_trading_user
+GO
+
+EXEC sp_addrolemember brady_trading_insert, brady_trading_user
+GO
+
 CREATE TABLE [brady_trading].[schemaversion](
 	[id] [bigint] IDENTITY(1,1) NOT NULL,
 	[version] [nvarchar](50) NOT NULL,
@@ -43,6 +358,9 @@ ALTER TABLE [brady_trading].[schemaversion]
    ADD CONSTRAINT [pk_dbversion]
       PRIMARY KEY CLUSTERED ( [id] ) 
          ON [PRIMARY]
+GO
+
+GRANT SELECT ON [brady_trading].[schemaversion] TO [brady_trading_select]
 GO
 
 CREATE PROCEDURE brady_trading.prc_checkversion
@@ -71,6 +389,9 @@ BEGIN
 END
 GO
 
+GRANT EXECUTE ON [brady_trading].[prc_checkversion] TO [brady_trading_execute]
+GO
+
 CREATE PROCEDURE brady_trading.prc_updateversion
     ( @NewMajor    INT
     , @NewMinor    INT
@@ -85,6 +406,9 @@ BEGIN
    INSERT INTO brady_trading.schemaversion (version, major, minor, patch, compile)
       VALUES (@version, @NewMajor, @NewMinor, @NewPatch, @NewCompile)
 END
+GO
+
+GRANT EXECUTE ON [brady_trading].[prc_updateversion] TO [brady_trading_execute]
 GO
 
 CREATE TABLE [brady_trading].[nexthigh]
@@ -104,6 +428,9 @@ GO
 ALTER TABLE [brady_trading].[nexthigh]
    ADD CONSTRAINT [uk_nexthigh1]
       UNIQUE (entityname)
+GO
+
+GRANT SELECT ON [brady_trading].[nexthigh] TO [brady_trading_select]
 GO
 
 CREATE TABLE [brady_trading].[trade](
@@ -152,13 +479,13 @@ GO
 
 CREATE TABLE [brady_trading].[commoditytrade](
 	[id] 				[bigint] 		NOT NULL,
-	[bs]				[nvarchar2](4),
-	[term]				[nvarchar2](64),
+	[bs]				[nvarchar](4),
+	[term]				[nvarchar](64),
 	[deliverymonth]		[datetime],
 	[deliverydate]		[datetime],
 	[lots]				[float],
 	[commodityamount]   [float],
-	[commodityunits]	[nvarchar2](64),
+	[commodityunits]	[nvarchar](64),
 ) ON [PRIMARY]
 GO
 
@@ -283,11 +610,11 @@ GO
 
 CREATE TABLE [brady_trading].[vanillaaverage](
 	[id] 					[bigint] 		NOT NULL,
-	[fixingindex]	    	[nvarchar2](64),
+	[fixingindex]	    	[nvarchar](64),
 	[startdate]		    	[datetime],
 	[enddate]		    	[datetime],
 	[additivepremium]      	[float],
-	[additivepremiumunits]	[nvarchar2](64),
+	[additivepremiumunits]	[nvarchar](64),
 	[percentagepremium] 	[float]
 ) ON [PRIMARY]
 GO
@@ -344,14 +671,14 @@ GO
 
 CREATE TABLE [brady_trading].[vanillaoption](
 	[id] 					[bigint] 		NOT NULL,
-	[cp]	    			[nvarchar2](4),
+	[cp]	    			[nvarchar](4),
 	[currencyamount]		[float],
-	[model]	    			[nvarchar2](64),
+	[model]	    			[nvarchar](64),
 	[strikeprice]			[float],
 	[expirymonth]		    [datetime],
 	[expirydate]		    [datetime],
 	[premiumdate]		    [datetime],
-	[premiumcurrency]	   	[nvarchar2](64),
+	[premiumcurrency]	   	[nvarchar](64),
 	[premiumrate]			[float],
 	[premiumamount] 		[float]
 ) ON [PRIMARY]
@@ -366,7 +693,7 @@ GO
 ALTER TABLE [brady_trading].[vanillaoption]
    ADD CONSTRAINT [fk_vanillaoption_optiondetails]
       FOREIGN KEY ([id])
-         REFERENCES [brady_trading].[vanillaoption]([id])
+         REFERENCES [brady_trading].[optiondetails]([id])
             ON DELETE CASCADE
 GO
 
@@ -397,7 +724,14 @@ GO
 ALTER TABLE [brady_trading].[commodityaverage]
    ADD CONSTRAINT [fk_commodityaverage_commoditytrade]
       FOREIGN KEY ([id])
-         REFERENCES [brady_trading].[commodityaverage]([id])
+         REFERENCES [brady_trading].[commoditytrade]([id])
+            ON DELETE CASCADE
+GO
+
+ALTER TABLE [brady_trading].[commodityaverage]
+   ADD CONSTRAINT [fk_commodityaverage_averagedetails]
+      FOREIGN KEY ([id])
+         REFERENCES [brady_trading].[averagedetails]([id])
             ON DELETE CASCADE
 GO
 
@@ -430,7 +764,14 @@ GO
 ALTER TABLE [brady_trading].[commodityaverageswap]
    ADD CONSTRAINT [fk_commodityaverageswap_commoditytrade]
       FOREIGN KEY ([id])
-         REFERENCES [brady_trading].[commodityaverageswap]([id])
+         REFERENCES [brady_trading].[commoditytrade]([id])
+            ON DELETE CASCADE
+GO
+
+ALTER TABLE [brady_trading].[commodityaverageswap]
+   ADD CONSTRAINT [fk_commodityaverageswap_averagedetails]
+      FOREIGN KEY ([id])
+         REFERENCES [brady_trading].[averagedetails]([id])
             ON DELETE CASCADE
 GO
 
@@ -461,7 +802,14 @@ GO
 ALTER TABLE [brady_trading].[commodityoption]
    ADD CONSTRAINT [fk_commodityoption_commoditytrade]
       FOREIGN KEY ([id])
-         REFERENCES [brady_trading].[commodityoption]([id])
+         REFERENCES [brady_trading].[commoditytrade]([id])
+            ON DELETE CASCADE
+GO
+
+ALTER TABLE [brady_trading].[commodityoption]
+   ADD CONSTRAINT [fk_commodityoption_optiondetails]
+      FOREIGN KEY ([id])
+         REFERENCES [brady_trading].[optiondetails]([id])
             ON DELETE CASCADE
 GO
 
@@ -493,7 +841,21 @@ GO
 ALTER TABLE [brady_trading].[commoditytapo]
    ADD CONSTRAINT [fk_commoditytapo_commoditytrade]
       FOREIGN KEY ([id])
-         REFERENCES [brady_trading].[commoditytapo]([id])
+         REFERENCES [brady_trading].[commoditytrade]([id])
+            ON DELETE CASCADE
+GO
+
+ALTER TABLE [brady_trading].[commoditytapo]
+   ADD CONSTRAINT [fk_commoditytapo_averagedetails]
+      FOREIGN KEY ([id])
+         REFERENCES [brady_trading].[averagedetails]([id])
+            ON DELETE CASCADE
+GO
+
+ALTER TABLE [brady_trading].[commoditytapo]
+   ADD CONSTRAINT [fk_commoditytapo_optiondetails]
+      FOREIGN KEY ([id])
+         REFERENCES [brady_trading].[optiondetails]([id])
             ON DELETE CASCADE
 GO
 
