72fbf0068e95409210ce7b020870a883df8e2502
darrenferne <darren.ferne@bradyplc.com>
2016-06-21 20:17:24 +0100

    wip

---
+++ TradeHub/Brady.Trade.DataService.TestHost/Config/Settings.cs
@@ -0,0 +1,17 @@
+﻿using System.Configuration;
+
+namespace Brady.Trade.DataService.TestHost.Config
+{
+    public class Settings
+    {
+        public readonly string ExplorerHostUrl;
+        public readonly string RootUrl;
+
+        public Settings()
+        {
+            var appSettingsReader = new AppSettingsReader();
+            ExplorerHostUrl = appSettingsReader.GetValue("ExplorerHostUrl", typeof(string)).ToString();
+            RootUrl = appSettingsReader.GetValue("RootUrl", typeof(string)).ToString();
+        }
+    }
+}
---
+++ TradeHub/Brady.Trade.DataService.TestHost/Program.cs
@@ -0,0 +1,36 @@
+﻿using Brady.Trade.DataService.TestHost.Config;
+using BWF.DataServices.StartUp.Concrete;
+using BWF.Explorer.StartUp.Concrete;
+using BWF.Explorer.StartUp.Interfaces;
+using BWF.Globalisation.Concrete;
+using BWF.Globalisation.Interfaces;
+using log4net.Config;
+using System;
+using System.Reflection;
+
+namespace Brady.Trade.DataService.TestHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var availableCultures = new Cultures();
+            var settings = new Settings();
+
+            XmlConfigurator.Configure();
+
+            var hostUrl = settings.ExplorerHostUrl;
+
+            IResourceQuerier resourceQuerier = new ResourceQuerier(typeof(BWF.Globalisation.Resources).Assembly);
+            var globalisationProvider = new GlobalisationProvider(resourceQuerier, availableCultures.LanguageCultures, availableCultures.FormattingCultures);
+            var currentDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            var menuFile = "Brady.Trade.DataService.TestHost.Menu.Menu.json";
+            var menuProvider = new JsonMenuProvider(menuFile, Assembly.GetExecutingAssembly());
+
+            IExplorerHostSettings explorerHostSettings = new ExplorerHostSettings("Brady.Trade.DataService.TestHost", "Brady Trade Data Service Host", "Host for the Brady Trade Data Service", hostUrl, menuProvider, globalisationProvider);
+            var explorerHost = new BWF.Explorer.StartUp.Concrete.ExplorerHost(explorerHostSettings);
+
+            explorerHost.Start();
+        }
+    }
+}
